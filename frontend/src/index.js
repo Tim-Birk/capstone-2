eff53d5a74c415b4d1e9f92bf8ba91aaad527fe1
Tim Birkmire <timbirkmire@yahoo.com>
2021-09-09 20:28:59 -0400

    Remove redundant store code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import * as models from './store/models';
-import { init } from '@rematch/core';
+import store from './store/store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const store = init({ models }); // Creates a redux store without the boilerplate
 
 ReactDOM.render(
   // <React.StrictMode>
